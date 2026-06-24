0b10f901e5740789f0f9d2a0e385bcfb25793d21
Axl-Ramses <ramses.coto1@gmail.com>
2026-06-23 18:49:17 -0600

    feat: instalar dependencias react-redux y reduxjs/toolkit

--- TechSpec/App.tsx
+++ TechSpec/App.tsx
@@ -1,20 +1,21 @@
+import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { navigationRef } from "./src/navigation/NavigationService";
 import { AuthProvider } from "./src/context/AuthContext";
 import { ThemeProvider } from "./src/context/ThemeContext";
-import { ComponentsProvider } from "./src/context/ComponentsContext";
+import { store } from "./src/store";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <ComponentsProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <AuthProvider>
           <NavigationContainer ref={navigationRef}>
             <StackNavigator />
           </NavigationContainer>
-        </ComponentsProvider>
-      </AuthProvider>
-    </ThemeProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
